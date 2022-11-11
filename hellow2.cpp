c99f92891098f3b2c7cd0f6bc830891b5fb15652
pantyetta <email@pantyetta.com>
2022-11-12 03:09:34 +0900

    console input

---
+++ hellow2.cpp
@@ -0,0 +1,15 @@
+// hellow2.cpp
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string name;
+
+    cout << "Hellow, I'm Computer." << endl;
+    cout << "Please Tell Me Your Name." << endl;
+    cin >> name;
+    cout << "Hi ! @" << name << endl;
+}
