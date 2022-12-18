be23d9b13cc98885adf7e297fe248206e7ad4f46
pantyetta <email@pantyetta.com>
2022-12-18 11:44:57 +0900

    use rand init

--- random.cpp
+++ random.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main(){
+    srand((unsigned)time(NULL));
+
     cout << rand() << endl;
     cout << rand() << endl;
     cout << rand() << endl;
